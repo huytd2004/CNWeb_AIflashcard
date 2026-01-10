525dc3ddd8627c6bf81ec5a22f79cf91d7f947e5
nghiaphunng <duynghia135z@gmail.com>
2026-01-10 13:05:17 +0700

    hotfix add header

--- frontend/src/layouts/AICenterLayout.tsx
+++ frontend/src/layouts/AICenterLayout.tsx
@@ -1,11 +1,14 @@
 import { SidebarProvider } from '@/components/ui/sidebar'
 import { AuthProvider } from '@/contexts/AuthContext'
 import { SideBarAICenter } from '@/features/app/ai-center/components/SideBarAICenter'
+import CFooter from '@/features/app/home/CFooter'
+import CHeader from '@/features/app/home/CHeader'
 import { Outlet } from 'react-router-dom'
 
 export default function AICenterLayout() {
     return (
         <div className="">
+            <CHeader />
             <AuthProvider>
                 <SidebarProvider>
                     <div className="flex min-h-screen w-full bg-gray-200/80  dark:bg-inherit">
@@ -13,8 +16,6 @@ export default function AICenterLayout() {
                         <main className="flex-1 overflow-auto py-6 ">
                             <Outlet />
                         </main>
-                        {/* <SidebarInset className="flex-1 bg-gray-200/80 dark:bg-inherit">
-                        </SidebarInset> */}
                     </div>
                 </SidebarProvider>
             </AuthProvider>
